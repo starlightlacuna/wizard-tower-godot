dafb64600c094929c8e27abe3ccba68473137fec
StarlightLacuna <169253492+starlightlacuna@users.noreply.github.com>
2025-06-29 16:58:45 -0400

    Add custom button resizing

--- Scenes/UI/custom_button.gd
+++ Scenes/UI/custom_button.gd
@@ -1,17 +1,38 @@
+@tool
 class_name CustomButton
 extends Control
 
 signal pressed
 
+@export var text: String = "":
+	set(new_text):
+		text = new_text
+		if Engine.is_editor_hint():
+			_resize()
+
 @onready var focus_texture: NinePatchRect = $FocusTexture
 @onready var button: Button = $Button
+@onready var button_frame: NinePatchRect = $Button/Frame
+@onready var button_label: Label = $Button/Label
+
 
 func _ready() -> void:
-	focus_texture.set_visible(false)
+	if not Engine.is_editor_hint():
+		focus_texture.set_visible(false)
+		_resize()
 
 
 func button_grab_focus() -> void:
 	button.grab_focus()
+
+
+func _resize() -> void:
+	button_label.set_text(text)
+	button_label.reset_size()
+	button_frame.set_size(button_label.get_size() + Vector2(9, 8))
+	focus_texture.set_size(button_frame.get_size() + Vector2(6, 6))
+	button.set_size(button_frame.get_size())
+	set_size(button.get_size())
 
 
 func _on_button_focus_entered() -> void:
