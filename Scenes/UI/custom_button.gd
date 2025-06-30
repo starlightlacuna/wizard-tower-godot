1b90cf25d9e8e9b762331c9f037421f47511f68a
StarlightLacuna <169253492+starlightlacuna@users.noreply.github.com>
2025-06-29 22:04:27 -0400

    Add custom button focus neighbor class members

--- Scenes/UI/custom_button.gd
+++ Scenes/UI/custom_button.gd
@@ -7,8 +7,16 @@ signal pressed
 @export var text: String = "":
 	set(new_text):
 		text = new_text
-		if Engine.is_editor_hint():
+		# Need to check that the node is ready, otherwise the label is null
+		if is_node_ready():
 			_resize()
+@export_category("Focus")
+@export var neighbor_left: NodePath
+@export var neighbor_top: NodePath
+@export var neighbor_right: NodePath
+@export var neighbor_bottom: NodePath
+@export var neighbor_next: NodePath
+@export var neighbor_previous: NodePath
 
 @onready var focus_texture: NinePatchRect = $FocusTexture
 @onready var button: Button = $Button
@@ -17,22 +25,26 @@ signal pressed
 
 
 func _ready() -> void:
+	_resize()
 	if not Engine.is_editor_hint():
 		focus_texture.set_visible(false)
-		_resize()
+		await get_tree().get_current_scene().ready
+		_set_focus_neighbors()
 
 
 func button_grab_focus() -> void:
 	button.grab_focus()
 
 
-func _resize() -> void:
-	button_label.set_text(text)
-	button_label.reset_size()
-	button_frame.set_size(button_label.get_size() + Vector2(9, 8))
-	focus_texture.set_size(button_frame.get_size() + Vector2(6, 6))
-	button.set_size(button_frame.get_size())
-	set_size(button.get_size())
+func _get_button_path(node_path: NodePath) -> NodePath:
+	#var node: Control = get_tree() \
+			#.get_current_scene() \
+			#.get_node_or_null(node_path)
+	var node: Control = get_node_or_null(node_path)
+	if node is CustomButton:
+		return node.button.get_path()
+	else:
+		return node_path
 
 
 func _on_button_focus_entered() -> void:
@@ -49,3 +61,32 @@ func _on_button_mouse_entered() -> void:
 
 func _on_button_pressed() -> void:
 	pressed.emit()
+
+
+func _resize() -> void:
+	button_label.set_text(text)
+	button_label.reset_size()
+	button_frame.set_size(button_label.get_size() + Vector2(9, 8))
+	focus_texture.set_size(button_frame.get_size() + Vector2(6, 6))
+	button.set_size(button_frame.get_size())
+	set_size(button.get_size())
+
+
+func _set_focus_neighbor(side: Side, node_path: NodePath) -> void:
+	button.set_focus_neighbor(side, _get_button_path(node_path))
+
+
+func _set_focus_neighbors() -> void:
+	if not neighbor_left.is_empty():
+		_set_focus_neighbor(SIDE_LEFT, neighbor_left)
+	if not neighbor_top.is_empty():
+		_set_focus_neighbor(SIDE_TOP, neighbor_top)
+	if not neighbor_right.is_empty():
+		_set_focus_neighbor(SIDE_RIGHT, neighbor_right)
+	if not neighbor_bottom.is_empty():
+		_set_focus_neighbor(SIDE_BOTTOM, neighbor_bottom)
+	if not neighbor_next.is_empty():
+		button.set_focus_next(_get_button_path(neighbor_next))
+	if not neighbor_previous.is_empty():
+		button.set_focus_previous(_get_button_path(neighbor_next))
+		
