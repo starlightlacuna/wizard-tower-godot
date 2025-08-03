a17bb0bfb57617d8b40c4c46d057eaf6f0485a0b
StarlightLacuna <169253492+starlightlacuna@users.noreply.github.com>
2025-08-03 15:13:27 -0400

    Add documentation for UI class

--- Scenes/UI/ui.gd
+++ Scenes/UI/ui.gd
@@ -1,24 +1,43 @@
 class_name UI
 extends Control
+## The UI scene for the game.
+##
+## This scene contains all the UI elements shown to the player during gameplay such as the tower
+## health bar, the pause menu, and the win and lose screens. It does not contain any game logic, nor
+## does it define any responses to input events. Instead, it emits signals that other classes can
+## connect and respond to.
 
+## Emitted when the No button in the lose window is pressed.
 signal lose_no_button_pressed
+
+## Emitted when the Yes button in the lose window is pressed.
 signal lose_yes_button_pressed
+
+## Emitted when the Back button in the pause menu is pressed.
 signal pause_menu_back_button_pressed
+
+## Emitted when the Quit button in the pause menu is pressed.
 signal pause_menu_quit_button_pressed
+
+## Emitted when the [code]pause_game[/code] input action is pressed.
 signal pause_game_pressed
+
+## Emitted when the No button in the win window is pressed.
 signal win_no_button_pressed
+
+## Emitted when the Yes button in the win window is pressed.
 signal win_yes_button_pressed
 
 @export var _tower_health_icon_scene: PackedScene
 
-## The current level.
-@export var current_level_value: int = 20:
+## The current level. This member is used for the level tracker.
+var current_level_value: int:
 	set(new_value):
 		current_level_value = new_value
 		_value_label.text = str(current_level_value)
 
-## The total number of levels.
-@export var total_level_value: int = 20:
+## The total number of levels. This member is used for the level tracker.
+var total_level_value: int:
 	set(new_value):
 		total_level_value = new_value
 		_total_label.text = str(total_level_value)
@@ -48,14 +67,16 @@ func _ready() -> void:
 func _shortcut_input(event: InputEvent) -> void:
 	if event.is_action_pressed("pause_game"):
 		if _pause_window.visible:
-			hide_pause_menu()
+			_hide_pause_menu()
 			pause_menu_back_button_pressed.emit()
 		else:
-			show_pause_window()
+			_show_pause_menu()
 			pause_game_pressed.emit()
 		return
 
 
+## Builds the tower's health bar. The health bar is composed of tower sprites, each representing 
+## two hit points. [param max_health] is used to determine how many sprites are created initially.
 func build_health_bar(max_health: int) -> void:
 	for child in _tower_health_bar.get_children():
 		child.queue_free()
@@ -67,11 +88,9 @@ func build_health_bar(max_health: int) -> void:
 		_tower_health_icons.append(new_icon)
 
 
-func hide_pause_menu() -> void:
-	_pause_window.visible = false
-
-
-func update_tower_health_bar(current_health: int, max_health) -> void:
+## Updates the tower's health bar. The health bar's individual icons are updated to the approprite 
+## sprites. See [method TowerHealthIcon.update].
+func update_tower_health_bar(current_health: int, max_health: int) -> void:
 	for index in _get_icon_count(max_health):
 		if current_health > 1:
 			_tower_health_icons[index].update(TowerHealthIcon.Display.FULL)
@@ -83,22 +102,17 @@ func update_tower_health_bar(current_health: int, max_health) -> void:
 			_tower_health_icons[index].update(TowerHealthIcon.Display.NONE)
 
 
+## Sets the level tracker to be visible.
 func show_level_tracker() -> void:
 	_value_label.visible = true
 	_slash_label.visible = true
 	_total_label.visible = true
 
 
-## Sets the lose window to be visible and makes it 
+## Sets the lose window to be visible and makes its Back button grab focus.
 func show_lose_window() -> void:
 	_lose_window.visible = true
 	_lose_window.get_node("YesButton").button_grab_focus.call_deferred()
-
-
-## Sets the pause window to be visible and makes its Back button grab focus.
-func show_pause_window() -> void:
-	_pause_window.visible = true
-	_pause_window.back_button_grab_focus_deferred()
 
 
 ## Sets the win window to visible and makes its Yes button grab focus.
@@ -112,12 +126,25 @@ func _get_icon_count(max_health: int) -> int:
 	return max_health / 2 if max_health % 2 == 0 else max_health / 2 + 1
 
 
+func _hide_pause_menu() -> void:
+	_pause_window.visible = false
+
+
 func _on_lose_no_button_pressed() -> void:
 	lose_no_button_pressed.emit()
 
 
 func _on_lose_yes_button_pressed() -> void:
 	lose_yes_button_pressed.emit()
+
+
+func _on_pause_window_back_button_pressed() -> void:
+	_hide_pause_menu()
+	pause_menu_back_button_pressed.emit()
+
+
+func _on_pause_window_quit_button_pressed() -> void:
+	pause_menu_quit_button_pressed.emit()
 
 
 func _on_win_no_button_pressed() -> void:
@@ -128,10 +155,6 @@ func _on_win_yes_button_pressed() -> void:
 	win_yes_button_pressed.emit()
 
 
-func _on_pause_window_back_button_pressed() -> void:
-	hide_pause_menu()
-	pause_menu_back_button_pressed.emit()
-
-
-func _on_pause_window_quit_button_pressed() -> void:
-	pause_menu_quit_button_pressed.emit()
+func _show_pause_menu() -> void:
+	_pause_window.visible = true
+	_pause_window.back_button_grab_focus_deferred()
