3dd6bf89a285e27be291bc2200910c0805053130
StarlightLacuna <169253492+starlightlacuna@users.noreply.github.com>
2025-08-03 20:14:13 -0400

    Add Firebolt VisibleOnScreenNotifier2D check

--- Scenes/Projectiles/firebolt.gd
+++ Scenes/Projectiles/firebolt.gd
@@ -2,16 +2,23 @@ class_name Firebolt
 extends CharacterBody2D
 ## A firebolt, the player's standard projectile attack.
 ##
-## Scenes using this script must have a VisibleOnScreenNotifier2D node so that they can be freed
-## when exiting the screen.
+## Scenes using this script must have a [VisibleOnScreenNotifier2D] node so that they can be freed
+## when exiting the screen.[br][br]
+## [i]Enhancement: Use an object pool to prevent creating and freeing firebolts.[/i]
 
 ## The speed of this firebolt in pixels per second.
 @export var speed: float = 100
 
 
 func _ready() -> void:
-	set_velocity(Vector2(speed, 0))
-	$VisibleOnScreenNotifier2D.screen_exited.connect(queue_free)
+	var notifier: VisibleOnScreenNotifier2D = null
+	for child in get_children():
+		if child is VisibleOnScreenNotifier2D:
+			notifier = child
+			break
+	assert(notifier, "[Firebolt] VisibleOnScreenNotifier2D doesn't exist!")
+	notifier.screen_exited.connect(queue_free)
+	velocity = Vector2(speed, 0)
 
 
 func _physics_process(_delta: float) -> void:
